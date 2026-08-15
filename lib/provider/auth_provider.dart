30fc4f530ca9208e80da53b3c52c3851adcea2ea
Aayush <aayushbastola772@gmail.com>
2026-08-15 23:23:09 +0545

    Feat: Logout

--- lib/provider/auth_provider.dart
+++ lib/provider/auth_provider.dart
@@ -4,71 +4,61 @@ import 'package:flutter/widgets.dart';
 import 'package:jwt_decoder/jwt_decoder.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-
-class AuthProvider extends ChangeNotifier{
-
-  
-
-    AuthProvider({AuthService? authService})
+class AuthProvider extends ChangeNotifier {
+  AuthProvider({AuthService? authService})
     : _authService = authService ?? AuthService();
 
-    final AuthService _authService;
+  final AuthService _authService;
 
-    bool isLoading = false;
-    String? errorMessage;
-    UserModel? user;
-    String? userToken;
+  bool isLoading = false;
+  String? errorMessage;
+  UserModel? user;
+  String? userToken;
 
+  Future<bool> login({required String email, required String password}) async {
+    isLoading = true;
+    errorMessage = null;
+    notifyListeners();
 
-Future<bool> login({
-  required String email, required String password
-})async {
-  isLoading = true;
-  errorMessage= null;
-  notifyListeners();
-
-  try{
-    final response = await _authService.login(email, password);
-    userToken = response.userToken;
-    final prefs= await SharedPreferences.getInstance(); // store the token in shared preferences
-    await prefs.setString('token', userToken!);
-    return true;
+    try {
+      final response = await _authService.login(email, password);
+      userToken = response.userToken;
+      final prefs =
+          await SharedPreferences.getInstance(); // store the token in shared preferences
+      await prefs.setString('token', userToken!);
+      return true;
+    } catch (err) {
+      errorMessage = err.toString();
+      return false;
+    } finally {
+      isLoading = false;
+      notifyListeners();
+    }
   }
-  catch(err){
-    errorMessage = err.toString();
-    return false;
+
+  Future<bool> isLoggedIn() async {
+    final prefs = await SharedPreferences.getInstance();
+    final token = prefs.getString('token');
+
+    if (token == null || token.isEmpty) {
+      userToken = null;
+      return false;
+    } else if (JwtDecoder.isExpired(token)) {
+      await prefs.remove('token');
+      userToken = null;
+      return false;
+    } else {
+      userToken = token;
+      return true;
+    }
   }
-  finally{
-    isLoading= false;
+
+  Future<void> logout() async {
+    userToken = null;
+    user = null;
+    errorMessage = null;
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove('token');
     notifyListeners();
   }
-
 }
-Future<bool> isLoggedIn() async{
-  final prefs= await SharedPreferences.getInstance();
-  final token= prefs.getString('token');
-
-  if(token==null || token.isEmpty){
-    userToken = null;
-    return false;
-  }
-  else if(JwtDecoder.isExpired(token)){
-    await prefs.remove('token');
-    userToken=null;
-    return false;
-  }
-  else{
-    userToken = token;
-    return true;
-  }
-}
-
-void logout(){
-  userToken=null;
-  user=null;
-  errorMessage = null;
-}
-
-  
-}
-
