6b43aeb607594622fd10b7e42b47d427b2499849
Aayush <aayushbastola772@gmail.com>
2026-08-15 19:01:11 +0545

    Check token and return to respective page

---
+++ lib/auth/auth_gate.dart
@@ -0,0 +1,53 @@
+import 'package:assignment_tracker/provider/auth_provider.dart';
+import 'package:assignment_tracker/screens/home_screen.dart';
+import 'package:assignment_tracker/screens/login_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class AuthGate extends StatefulWidget {
+  const AuthGate({super.key});
+
+  @override
+  State<AuthGate> createState() => _AuthGateState();
+}
+
+class _AuthGateState extends State<AuthGate> {
+bool? isLoggedIn;
+
+@override
+  void initState() {
+
+    super.initState();
+    _checkAuth();
+  }
+
+  Future<void> _checkAuth() async{
+    final auth = context.read<AuthProvider>();
+    final result = await auth.isLoggedIn();
+
+    if(!mounted) return;
+
+    setState(() {
+      isLoggedIn= result;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (isLoggedIn==null){
+      return  Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(),
+        ),
+      );
+    }
+    if(isLoggedIn!){
+      return const HomeScreen();
+
+
+
+    }
+    return const LoginScreen();
+    
+  }
+}
