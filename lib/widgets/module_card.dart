935685905400d58b1ff0803227ac3660243c388c
Aayush <aayushbastola772@gmail.com>
2026-08-17 00:28:53 +0545

    ADD: gesture detector/ navigate to module assignments

--- lib/widgets/module_card.dart
+++ lib/widgets/module_card.dart
@@ -1,40 +1,50 @@
 import 'package:assignment_tracker/custom/custom_text.dart';
+import 'package:assignment_tracker/screens/module_assignment_list_screen.dart';
 import 'package:flutter/material.dart';
 
 class ModuleCard extends StatelessWidget {
   final String moduleName;
   final String pendingAssignments;
+  final int moduleId;
   const ModuleCard({
     super.key,
+  
     required this.moduleName,
     required this.pendingAssignments,
+    required this.moduleId,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        border: Border.all(width: 1),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [Icon(Icons.book), Icon(Icons.more_vert)],
-            ),
-            SizedBox(height: 20),
-            CustomText(text: moduleName),
-            SizedBox(height: 5),
-            CustomText(
-              text: int.tryParse(pendingAssignments)! > 1
-                  ? "$pendingAssignments Assignments pending"
-                  : "$pendingAssignments Assignment pending",
-            ),
-          ],
+    return GestureDetector(
+      onTap: () {
+        // Navigate to the module assignment list screen
+        Navigator.push(context, MaterialPageRoute(builder: (context) => ModuleAssignmentListScreen()));
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(width: 1),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [Icon(Icons.book), Icon(Icons.more_vert)],
+              ),
+              SizedBox(height: 20),
+              CustomText(text: moduleName),
+              SizedBox(height: 5),
+              CustomText(
+                text: int.tryParse(pendingAssignments)! > 1
+                    ? "$pendingAssignments Assignments pending"
+                    : "$pendingAssignments Assignment pending",
+              ),
+            ],
+          ),
         ),
       ),
     );
