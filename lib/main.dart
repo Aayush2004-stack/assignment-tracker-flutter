c1c10d26cdc61ead72d43a54ca5a26bfed3e6711
Aayush <aayushbastola772@gmail.com>
2026-08-16 15:36:24 +0545

    ADD: module provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:assignment_tracker/app/assignment_tracker.dart';
 import 'package:assignment_tracker/provider/auth_provider.dart';
+import 'package:assignment_tracker/provider/module_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 void main() {
-  runApp(MultiProvider(
-    providers: [ChangeNotifierProvider(create: (_)=>AuthProvider())],
-    child: const AssignmentTracker()));
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthProvider()),
+        ChangeNotifierProvider(create: (_) => ModuleProvider()),
+      ],
+      child: const AssignmentTracker(),
+    ),
+  );
 }
