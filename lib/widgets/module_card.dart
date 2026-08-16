ee726fe595f7855d9a8b04c499a9f4511428619c
Aayush <aayushbastola772@gmail.com>
2026-08-16 17:10:15 +0545

    UPDATE: dynamically keep module details

--- lib/widgets/module_card.dart
+++ lib/widgets/module_card.dart
@@ -2,7 +2,13 @@ import 'package:assignment_tracker/custom/custom_text.dart';
 import 'package:flutter/material.dart';
 
 class ModuleCard extends StatelessWidget {
-  const ModuleCard({super.key});
+  final String moduleName;
+  final String pendingAssignments;
+  const ModuleCard({
+    super.key,
+    required this.moduleName,
+    required this.pendingAssignments,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -21,9 +27,13 @@ class ModuleCard extends StatelessWidget {
               children: [Icon(Icons.book), Icon(Icons.more_vert)],
             ),
             SizedBox(height: 20),
-            CustomText(text: "Module Name"),
+            CustomText(text: moduleName),
             SizedBox(height: 5),
-            CustomText(text: "1 Assignment pending"),
+            CustomText(
+              text: int.tryParse(pendingAssignments)! > 1
+                  ? "$pendingAssignments Assignments pending"
+                  : "$pendingAssignments Assignment pending",
+            ),
           ],
         ),
       ),
