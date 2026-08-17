d7c676df37d72940cc980f74ce578338e1d28633
Aayush <aayushbastola772@gmail.com>
2026-08-17 16:31:52 +0545

    UPDATE: dynamically keep assignments details

--- lib/screens/assignment_details_screen.dart
+++ lib/screens/assignment_details_screen.dart
@@ -1,7 +1,9 @@
 import 'package:assignment_tracker/custom/custom_text.dart';
+import 'package:assignment_tracker/provider/assignment_provider.dart';
 import 'package:assignment_tracker/provider/task_provider.dart';
 import 'package:assignment_tracker/widgets/task_card.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 
 class AssignmentDetailsScreen extends StatefulWidget {
@@ -23,6 +25,9 @@ class _AssignmentDetailsScreenState extends State<AssignmentDetailsScreen> {
       context.read<TaskProvider>().fetchTasks(
         assignmentId: widget.assignmentId,
       );
+      context.read<AssignmentProvider>().fetchAssignmentDetails(
+        widget.assignmentId,
+      );
     });
   }
 
@@ -33,88 +38,101 @@ class _AssignmentDetailsScreenState extends State<AssignmentDetailsScreen> {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              CustomText(text: "Module: Module Name"),
-              SizedBox(height: 10),
-              CustomText(text: "Assignment Name"),
-              SizedBox(height: 20),
-              CustomText(
-                text: "Description: This is a sample assignment description.",
-              ),
-              SizedBox(height: 20),
-              CustomText(text: "Deadline: 2023-12-31"),
-              SizedBox(height: 20),
-              Container(
-                alignment: Alignment.center,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  border: Border.all(width: 1),
-                  borderRadius: BorderRadius.circular(16),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(16),
-                  child: Column(
-                    // crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Stack(
-                        alignment: Alignment.center,
-
-                        children: [
-                          SizedBox(
-                            width: 60,
-
-                            height: 60,
-
-                            child: CircularProgressIndicator(
-                              strokeWidth: 10,
-                              backgroundColor: Colors.grey[300],
-                              value: 0.68,
-                            ),
-                          ),
-
-                          Text('${((0.68) * 100).round()}%'),
-                        ],
-                      ),
-                      SizedBox(height: 10),
-                      CustomText(text: "Overall Progress"),
-                      SizedBox(height: 10),
-                      CustomText(
-                        text:
-                            "You are in track with your assignment progress. Keep up the good work!",
-                      ),
-                      SizedBox(height: 20),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          ElevatedButton(onPressed: () {}, child: Text('Edit')),
-                          SizedBox(width: 10),
-                          ElevatedButton(
-                            onPressed: () {},
-                            child: Text('Add Task'),
-                          ),
-                        ],
-                      ),
-                    ],
+          child: Consumer2<AssignmentProvider, TaskProvider>(
+            builder: (context, assignmentProvider, taskProvider, _) {
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CustomText(
+                    text:
+                        "Module: ${assignmentProvider.assignment?.moduleName ?? 'Loading...'}",
                   ),
-                ),
-              ),
-              SizedBox(height: 20),
-              CustomText(text: "Tasks"),
-              SizedBox(height: 10),
-              Consumer<TaskProvider>(
-                builder: (context, provider, _) {
-                  if (provider.isLoading) {
-                    return Center(child: CircularProgressIndicator());
-                  } else if (provider.tasks.isEmpty) {
-                    return Center(child: Text("No tasks available"));
-                  } else {
-                    return Expanded(
+                  SizedBox(height: 10),
+                  CustomText(
+                    text: assignmentProvider.assignment?.title ?? 'Loading...',
+                  ),
+                  SizedBox(height: 20),
+                  CustomText(
+                    text:
+                        "Description: ${assignmentProvider.assignment?.details ?? 'Loading...'}",
+                  ),
+                  SizedBox(height: 20),
+                  CustomText(
+                    text:
+                        "Deadline: ${DateFormat('dd MMM yyyy').format(assignmentProvider.assignment?.deadline ?? DateTime.now())}",
+                  ),
+                  SizedBox(height: 20),
+                  Container(
+                    alignment: Alignment.center,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      border: Border.all(width: 1),
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(16),
+                      child: Column(
+                        // crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Stack(
+                            alignment: Alignment.center,
+
+                            children: [
+                              SizedBox(
+                                width: 60,
+
+                                height: 60,
+
+                                child: CircularProgressIndicator(
+                                  strokeWidth: 10,
+                                  backgroundColor: Colors.grey[300],
+                                  value: 0.68,
+                                ),
+                              ),
+
+                              Text('${((0.68) * 100).round()}%'),
+                            ],
+                          ),
+                          SizedBox(height: 10),
+                          CustomText(text: "Overall Progress"),
+                          SizedBox(height: 10),
+                          CustomText(
+                            text:
+                                "You are in track with your assignment progress. Keep up the good work!",
+                          ),
+                          SizedBox(height: 20),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              ElevatedButton(
+                                onPressed: () {},
+                                child: Text('Edit'),
+                              ),
+                              SizedBox(width: 10),
+                              ElevatedButton(
+                                onPressed: () {},
+                                child: Text('Add Task'),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  CustomText(text: "Tasks"),
+                  SizedBox(height: 10),
+                  if (taskProvider.isLoading)
+                    Center(child: CircularProgressIndicator())
+                  else if (taskProvider.tasks.isEmpty)
+                    Center(child: Text("No tasks available"))
+                  else
+                    Expanded(
                       child: ListView.separated(
-                        itemCount: provider.tasks.length,
+                        itemCount: taskProvider.tasks.length,
                         itemBuilder: (context, index) {
-                          final task = provider.tasks[index];
+                          final task = taskProvider.tasks[index];
+
                           return TaskCard(
                             taskName: task.taskTitle,
                             taskId: task.taskId,
@@ -124,11 +142,10 @@ class _AssignmentDetailsScreenState extends State<AssignmentDetailsScreen> {
                         separatorBuilder: (context, index) =>
                             SizedBox(height: 10),
                       ),
-                    );
-                  }
-                },
-              ),
-            ],
+                    ),
+                ],
+              );
+            },
           ),
         ),
       ),
