6e1ed10c1bf95bdf827531435773ce4640068d29
Aayush <aayushbastola772@gmail.com>
2026-08-17 16:35:40 +0545

    FEAT: get assignments from db

--- lib/screens/module_assignment_list_screen.dart
+++ lib/screens/module_assignment_list_screen.dart
@@ -1,12 +1,23 @@
 import 'package:assignment_tracker/custom/custom_text.dart';
+import 'package:assignment_tracker/provider/assignment_provider.dart';
 import 'package:assignment_tracker/widgets/assignment_card.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
 
 class ModuleAssignmentListScreen extends StatelessWidget {
-  const ModuleAssignmentListScreen({super.key});
+  final int moduleId;
+  final String moduleName;
+  const ModuleAssignmentListScreen({
+    super.key,
+    required this.moduleId,
+    required this.moduleName,
+  });
 
   @override
   Widget build(BuildContext context) {
+    context.read<AssignmentProvider>().fetchModuleAssignments(moduleId);
+
     return Scaffold(
       appBar: AppBar(title: Text('Module Assignments')),
       body: SafeArea(
@@ -15,20 +26,37 @@ class ModuleAssignmentListScreen extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              CustomText(text: "Module Name"),
-              SizedBox(height: 10),
-              AssignmentCard(
-                assignmentName: "Assignment 1",
-                moduleName: "Module Name",
-                dueDate: "2023-12-31",
-                assignmentId: 1,
-              ),
-              SizedBox(height: 10),
-              AssignmentCard(
-                assignmentName: "Assignment 2",
-                moduleName: "Module Name",
-                dueDate: "2024-01-15",
-                assignmentId: 2,
+              CustomText(text: "Assignments for module: $moduleName"),
+              SizedBox(height: 20),
+
+              Expanded(
+                child: Consumer<AssignmentProvider>(
+                  builder: (context, assignmentProvider, _) {
+                    if (assignmentProvider.isLoading) {
+                      return Center(child: CircularProgressIndicator());
+                    } else if (assignmentProvider.moduleAssignments.isEmpty) {
+                      return Center(
+                        child: Text('No assignments found for module:'),
+                      );
+                    } else {
+                      return ListView.builder(
+                        itemCount: assignmentProvider.moduleAssignments.length,
+                        itemBuilder: (context, index) {
+                          final assignment =
+                              assignmentProvider.moduleAssignments[index];
+                          return AssignmentCard(
+                            assignmentId: assignment.assignmentId,
+                            assignmentName: assignment.title,
+                            moduleName: assignment.moduleName,
+                            dueDate: DateFormat(
+                              'dd MMM yyyy',
+                            ).format(assignment.deadline).toString(),
+                          );
+                        },
+                      );
+                    }
+                  },
+                ),
               ),
             ],
           ),
