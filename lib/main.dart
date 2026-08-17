6672a5ff291522628a0754a48e49d6b1a54a04de
Aayush <aayushbastola772@gmail.com>
2026-08-17 14:43:30 +0545

    ADD: task provider in multiProvider

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:assignment_tracker/app/assignment_tracker.dart';
 import 'package:assignment_tracker/provider/assignment_provider.dart';
 import 'package:assignment_tracker/provider/auth_provider.dart';
 import 'package:assignment_tracker/provider/module_provider.dart';
+import 'package:assignment_tracker/provider/task_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -12,6 +13,7 @@ void main() {
         ChangeNotifierProvider(create: (_) => AuthProvider()),
         ChangeNotifierProvider(create: (_) => ModuleProvider()),
         ChangeNotifierProvider(create: (_) => AssignmentProvider()),
+        ChangeNotifierProvider(create: (_) => TaskProvider()),
       ],
       child: const AssignmentTracker(),
     ),
